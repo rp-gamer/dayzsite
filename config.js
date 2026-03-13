43dbb90d22e9222baa616515a4b5acb28e24c9ba
rp-gamer <54762088+rp-gamer@users.noreply.github.com>
2026-03-13 12:43:23 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -5,5 +5,5 @@
 // 2. Settings → API
 // 3. Copia "Project URL" e "anon public"
 
-const SUPABASE_URL = 'https://xzkvpaokqbdjlsozjqut.supabase.co';   // ex: https://xxxx.supabase.co
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6a3ZwYW9rcWJkamxzb3pqcXV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMzNDExMTksImV4cCI6MjA4ODkxNzExOX0.Av2vtyeQ6hpX9r_0clJWDeOFsY6h74RZ63l5-3Fw7zI';   // ex: eyJhbGci...
+const SUPABASE_URL = '';   // ex: https://xxxx.supabase.co
+const SUPABASE_KEY = '';   // ex: eyJhbGci...
